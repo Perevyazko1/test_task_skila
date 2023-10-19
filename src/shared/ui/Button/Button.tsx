05b8f323a9b48604c273cccd00a6ef260e4ae000
Perevyazko <a.perevyazko@gmail.com>
2023-10-19 21:10:30 +0300

    add button navbar

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,42 @@
+import {ButtonHTMLAttributes, memo, ReactNode} from 'react';
+
+import {classNames, Mods} from "shared/lib/classNames/classNames";
+
+import cls from "./Button.module.scss";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    outline?: boolean
+    className?: string
+    children?: ReactNode
+    iconButton:string
+}
+
+
+export const Button = memo((props: ButtonProps) => {
+    const {
+        outline = false,
+        className,
+        iconButton,
+        children,
+        ...otherProps
+    } = props
+
+    const mods: Mods = {
+        [cls.outline]: outline,
+    };
+
+    return (
+        <button
+            className={classNames(cls.Button, mods, [className])}
+            {...otherProps}
+        >
+            {iconButton &&
+                <div className={cls.IconButton}>
+                    <img  src={iconButton}/>
+                </div>
+
+            }
+            {children}
+        </button>
+    );
+});
--- src/widgets/NavBarCustom/NavBarCustom.tsx
+++ src/widgets/NavBarCustom/NavBarCustom.tsx
@@ -2,18 +2,19 @@ import {memo, ReactNode} from 'react';
 import {classNames, Mods} from "shared/lib/classNames/classNames";
 import cls from "./NavBar.module.scss"
 import calls from "shared/assets/icons/calls.svg"
-import counterparties from "../../shared/assets/icons/counterparties.svg"
-import knowledge_base from "../../shared/assets/icons/knowledge_base.svg"
-import orders from "../../shared/assets/icons/orders.svg"
-import performers from "../../shared/assets/icons/performers.svg"
-import reports from "../../shared/assets/icons/reports.svg"
-import results from "../../shared/assets/icons/results.svg"
-import settings from "../../shared/assets/icons/settings.svg"
-import messages from "../../shared/assets/icons/messages.svg"
-import documents from "../../shared/assets/icons/documents.svg"
-import { ListLink } from 'shared/ui/ListLink/ListLink';
+import counterparties from "shared/assets/icons/counterparties.svg"
+import knowledge_base from "shared/assets/icons/knowledge_base.svg"
+import orders from "shared/assets/icons/orders.svg"
+import performers from "shared/assets/icons/performers.svg"
+import reports from "shared/assets/icons/reports.svg"
+import results from "shared/assets/icons/results.svg"
+import settings from "shared/assets/icons/settings.svg"
+import messages from "shared/assets/icons/messages.svg"
+import documents from "shared/assets/icons/documents.svg"
 import logo from "shared/assets/icons/logo.svg"
-
+import {Button} from "shared/ui/Button/Button";
+import alert from "shared/assets/icons/alert.svg"
+import plus from "shared/assets/icons/plus.svg"
 
 
 interface NavBarProps {
@@ -79,6 +80,10 @@ export const NavBarCustom = memo((props: NavBarProps) => {
                 <div className={cls.Link}>
                     <img className={cls.IconLink} src={settings}/>
                     <span className={cls.TextLink}>Настройки</span>
+                </div>
+                <div className={cls.ContainerButton}>
+                    <Button className={cls.Button} iconButton={plus}>Добавить заказ</Button>
+                    <Button className={cls.Button} iconButton={alert}>Оплата</Button>
                 </div>
 
 
